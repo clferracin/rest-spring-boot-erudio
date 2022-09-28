a41cf141454ab913099489981a8dacd516a5ce1b
Cesar Ferracin <cesarferracin@gmail.com>
2022-09-27 21:25:18 -0300

    initial steps with java and spring boot 3

---
+++ src/test/java/com/noctua/RestSpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noctua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
